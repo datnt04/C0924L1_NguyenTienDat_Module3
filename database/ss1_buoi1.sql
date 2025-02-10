23955495ee7b7047b8f02420d283b56e58c68924
datnt04 <ndat020304@gmail.com>
2025-02-10 21:06:37 +0700

    update

--- database/ss1_buoi1.sql
+++ database/ss1_buoi1.sql
@@ -1,23 +1,23 @@
 CREATE DATABASE student_management;
 USE student_management;
-CREATE TABLE Class (
+CREATE TABLE class (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE Teacher (
+CREATE TABLE teacher (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(50) NOT NULL,
     age INT NOT NULL,
     country VARCHAR(50) NOT NULL
 );
-INSERT INTO Class (name)
+INSERT INTO class (name)
 VALUES 
     ('Lớp 10A'),
     ('Lớp 11B');
-INSERT INTO Teacher (name, age, country)
+INSERT INTO teacher (name, age, country)
 VALUES 
     ('Nguyễn Văn A', 40, 'Vietnam'),
     ('Trần Thị B', 30, 'Vietnam');
-    SELECT * FROM Class;
-SELECT * FROM Teacher;
+    SELECT * FROM class;
+SELECT * FROM teacher;
